b0b817757258e122455e63ae3f719e6eb6083c3c
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-12-05 22:13:09 +0530

    challange 2 id

--- src/App.js
+++ src/App.js
@@ -7,23 +7,32 @@ export default function App() {
       <h1>
         mark 14 stocks
         <main>
-          <label for="">
+          <label for="initial-price">
             initial price{" "}
-            <input type="text" placeholder="enter initial price"></input>
+            <input
+              id="initial-price"
+              type="text"
+              placeholder="enter initial price"
+            ></input>
           </label>
-          <label for="">
+          <label for="quantity-of-stock">
             quantity of stocks{" "}
             <input
+              id="quantity-of-stock"
               type="text"
               placeholder="enter the number of stocks you bought"
             ></input>
           </label>
-          <label for="">
+          <label for="current-price">
             current price{" "}
-            <input type="text" placeholder="enter current price"></input>
+            <input
+              id="current-price"
+              type="text"
+              placeholder="enter current price"
+            ></input>
           </label>
 
-          <button>tell me</button>
+          <button id="submit-btn">tell me</button>
 
           <div></div>
         </main>
